3ffb7711a479c46c897bfeeefe7aefb16bc6a271
benjaminpolydeq <kbenjio9@gmail.com>
2026-02-12 13:04:52 +0000

    Update crud.py

--- backend/crud.py
+++ backend/crud.py
@@ -1,1 +1,9 @@
-crud
+from sqlalchemy.orm import Session
+from models import Contribution
+
+def create_contribution(db: Session, user_id: int, category: str, type: str, file_path: str):
+    db_contribution = Contribution(user_id=user_id, category=category, type=type, file_path=file_path)
+    db.add(db_contribution)
+    db.commit()
+    db.refresh(db_contribution)
+    return db_contribution
