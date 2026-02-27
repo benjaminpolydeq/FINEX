9c72696a32ff0abd8bc50425570168d6ec91c075
benjaminpolydeq <kbenjio9@gmail.com>
2026-02-27 20:53:04 +0000

    Create database.py for database connection configuration

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,2 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/finex_db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# database.py
+# Configure la connexion à la base de données et l'initialisation (ex : SQLAlchemy engine, session).
